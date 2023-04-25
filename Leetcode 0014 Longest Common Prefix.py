f093e9834f8c371413de8d030146e4c10ba5460f
Will <66586152+gostjoke@users.noreply.github.com>
2023-04-25 01:55:08 -0700

    Update Leetcode 0014 Longest Common Prefix.py

--- Leetcode 0014 Longest Common Prefix.py
+++ Leetcode 0014 Longest Common Prefix.py
@@ -8,4 +8,13 @@ class Solution:
         return ("".join(a))
 
 a = Solution().longestCommonPrefix(strs = ["flower","flow","flight"])
-print(a)
+
+#####  faster
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        a=""
+        for i in zip(*strs):
+            if len(set(i)) != 1:
+                break
+            a += i[0]
+        return a
