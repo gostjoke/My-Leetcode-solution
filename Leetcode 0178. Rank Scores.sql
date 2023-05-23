03431d92f6045c3e2dbccb83caa099e6c7d4ba8c
Will <66586152+gostjoke@users.noreply.github.com>
2023-05-23 16:12:05 -0700

    Update Leetcode 0178. Rank Scores.sql

--- Leetcode 0178. Rank Scores.sql
+++ Leetcode 0178. Rank Scores.sql
@@ -10,3 +10,11 @@
 SELECT s.Score as score,
 Dense_RANK() OVER (ORDER BY s.Score DESC) as `rank`
 FROM Scores s
+
+## second way
+Select s1.score , s2.`rank` from Scores s1
+left join 
+(select s2.score, rank() over(order by score desc) as `rank` from
+(SELECT DISTINCT score FROM Scores) s2) as s2
+on s1.score = s2.score
+order by `rank`
