535479293895689faf957da7cfbf487f93151a9b
Will <66586152+gostjoke@users.noreply.github.com>
2023-05-23 15:04:12 -0700

    Add files via upload

---
+++ Leetcode 1068 Product Sales Analysis I.sql
@@ -0,0 +1,17 @@
+'''
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+
+@file         :Leetcode 0034 Find First and Last Position of Element in Sorted Array.py
+@explain      :
+@date         :2023/05/23 15:03:55
+@author       :Tien-Wei Hsu
+@version      :1.0
+'''
+
+
+# Write your MySQL query statement below
+
+select p.product_name, s.year, s.price from Sales s
+left join Product p
+on s.product_id = p.product_id
