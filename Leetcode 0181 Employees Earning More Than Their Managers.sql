c93eb7f10c07aaa1630fcae127cf76b9e97f5368
Will <66586152+gostjoke@users.noreply.github.com>
2023-05-23 14:58:44 -0700

    Add files via upload

---
+++ Leetcode 0181 Employees Earning More Than Their Managers.sql
@@ -0,0 +1,17 @@
+'''
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+
+@file         :Leetcode 0034 Find First and Last Position of Element in Sorted Array.py
+@explain      :
+@date         :2023/05/23 14:57:55
+@author       :Tien-Wei Hsu
+@version      :1.0
+'''
+
+
+# Write your MySQL query statement below
+select e2.name as Employee from employee e1
+inner join employee e2
+on e1.id = e2.managerId
+where e1.salary < e2.salary
