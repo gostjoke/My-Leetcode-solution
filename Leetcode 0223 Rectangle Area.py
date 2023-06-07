86ad9947fce98cb3936165744c75682f124fce60
Will <66586152+gostjoke@users.noreply.github.com>
2023-06-07 11:09:17 -0700

    Update Leetcode 0223 Rectangle Area.py

--- Leetcode 0223 Rectangle Area.py
+++ Leetcode 0223 Rectangle Area.py
@@ -10,8 +10,8 @@
 # Definition for singly-linked list.
 class Solution:
     def computeArea(self, ax1: int, ay1: int, ax2: int, ay2: int, bx1: int, by1: int, bx2: int, by2: int) -> int:
-        rec1 = abs(ax2 - ax1) * abs(ay2-ay1)
-        rec2 = abs(bx2 - bx1) * abs(by2-by1)
+        rec1 = abs((ax2 - ax1) * (ay2-ay1))
+        rec2 = abs((bx2 - bx1) * (by2-by1))
         # 4 coner (ax1,ay1) (ax2,ay1) (ax2,ay2) (ax1,ay2)
 
         overlap_width = min(ax2,bx2) - max(ax1, bx1)
