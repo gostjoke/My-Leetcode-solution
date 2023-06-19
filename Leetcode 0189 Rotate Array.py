2f43d45e78449df41668f5b67fbb82d232c32cba
Will <66586152+gostjoke@users.noreply.github.com>
2023-06-19 15:36:07 -0700

    Create Leetcode 0189 Rotate Array.py

---
+++ Leetcode 0189 Rotate Array.py
@@ -0,0 +1,13 @@
+'''
+6/19/2023
+'''
+
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        for i in range(k):
+            nums.insert(0,nums[-1])
+            nums.pop()
+        
