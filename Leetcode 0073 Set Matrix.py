bc04bd4dbdbdd0a0ae0200a1d570ecf6838d500c
Will <66586152+gostjoke@users.noreply.github.com>
2023-06-19 14:06:02 -0700

    Update Leetcode 0073 Set Matrix.py

--- Leetcode 0073 Set Matrix.py
+++ Leetcode 0073 Set Matrix.py
@@ -5,19 +5,17 @@
 
 class Solution:
     def setZeroes(self, matrix: List[List[int]]) -> None:
-        coor = {"x":[], "y":[])
+        coor = {"x":[], "y":[]}
         ## To find their X, y coor then make in same row and col 0
         for i in range(len(matrix)): 
             for j in range(len(matrix[i])):
                 if matrix[i][j] == 0:
-                    coor["x"].append(f"{i}")
-                    coor["y"].append(f"{j}")
+                    coor["x"].append(i)
+                    coor["y"].append(j)
         
         for x in coor['x']:
-            x = int(x)
             for col in range(len(matrix[x])):
                 matrix[x][col] = 0
         for y in coor['y']:
-            y = int(y)
             for row in range(len(matrix)):
                 matrix[row][y] = 0
