d4cbd176840a01b77143830d8e5ed79f2cdeafb0
Will <66586152+gostjoke@users.noreply.github.com>
2023-06-21 12:55:24 -0700

    Create Leetcode 0078 Subsets.py

---
+++ Leetcode 0078 Subsets.py
@@ -0,0 +1,26 @@
+'''
+2023/06/21
+thanks to someone's helping to understand
+'''
+
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        res = []
+
+        subset = []
+
+        def dfs(i):
+            if i >= len(nums):
+                res.append(subset.copy())
+                return
+
+            # decision to include nums[i]
+            subset.append(nums[i])
+            dfs(i + 1)
+
+            # decision Not to include nums[i]
+            subset.pop()
+            dfs(i + 1)
+
+        dfs(0)
+        return res
