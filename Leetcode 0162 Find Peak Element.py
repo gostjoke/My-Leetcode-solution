a835a5ae1246ae40bb0d8cb64d591c78aaa98a55
Will <66586152+gostjoke@users.noreply.github.com>
2023-06-21 11:30:27 -0700

    Update Leetcode 0162 Find Peak Element.py

--- Leetcode 0162 Find Peak Element.py
+++ Leetcode 0162 Find Peak Element.py
@@ -1,4 +1,6 @@
 '''
+06/21
+use binary search to do it
 '''
 
 class Solution:
@@ -7,10 +9,22 @@ class Solution:
         left = 0
         right = len(nums)-1
         while left < right:
-            mid = ((right+left) // 2 )
+            mid = (right+left) // 2 
             if nums[mid] > nums[mid+1]:
                 right = mid
             else:
                 left = mid + 1
         
         return left
+
+# my first solution passed but not good, use extra space
+
+class Solution:
+    def findPeakElement(self, nums: List[int]) -> int:
+        peak = (-2)**31
+        index = 0
+        for ind, i in enumerate(nums):
+            if i > peak:
+                peak =i
+                index = ind
+        return index
