8d4078f5cb410245cc934861c057c054fd165387
Will <66586152+gostjoke@users.noreply.github.com>
2023-06-22 16:51:13 -0700

    Update Leetcode 0070 Climbing Stairs.py

--- Leetcode 0070 Climbing Stairs.py
+++ Leetcode 0070 Climbing Stairs.py
@@ -15,9 +15,24 @@ dp = [1, 1, 2, 3, 5, 0]
 dp[5]：
 dp = [1, 1, 2, 3, 5, 8]
 '''
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        dp = [0] * (n+1)
+        dp[0], dp[1] = 1 , 1
+        if len(dp) <= 2:
+            return 1
+        counter = 2
+        for i in dp[2:]:
+            dp[counter] = dp[counter-1] + dp[counter-2]
+            counter += 1 
+        return dp[-1]
 
-one, two = 1,1
-for i in range(n-1):
-    print(one, two)
-    one, two = two, one + two
-return two
+### better solution
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        one, two = 1,1
+        
+        for i in range(n-1):
+            print(one, two)
+            one, two = two, one + two
+        return two
