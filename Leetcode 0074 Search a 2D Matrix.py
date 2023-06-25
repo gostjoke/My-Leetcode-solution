37e23968808fb16f0c748bd26448ee47d01ef0a3
Will <66586152+gostjoke@users.noreply.github.com>
2023-06-24 20:31:50 -0700

    Update Leetcode 0074 Search a 2D Matrix.py

--- Leetcode 0074 Search a 2D Matrix.py
+++ Leetcode 0074 Search a 2D Matrix.py
@@ -1,6 +1,23 @@
 """
 06/24/2023
 """
+### better solution
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        m = len(matrix) - 1
+        n = len(matrix[0]) - 1
+        x,y = 0,0
+        while True:
+            if matrix[y][x] == target:
+                return True
+            elif matrix[y][x] != target and x < n:
+                x += 1
+            elif matrix[y][x] != target and x == n and y < m:
+                y += 1
+                x = 0
+            elif y == m and x == n and matrix[y][x] != target:
+                return False
+
 
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
