24bbde20a56316e6daf979ab149800a48b5ae032
Will <66586152+gostjoke@users.noreply.github.com>
2023-08-08 03:35:09 -0700

    Update Leetcode 0326 Power of Three.py

--- Leetcode 0326 Power of Three.py
+++ Leetcode 0326 Power of Three.py
@@ -1,5 +1,6 @@
 """
 08/08/2023
+this can use in any powerofnumber
 """
 
 class Solution:
@@ -10,6 +11,20 @@ class Solution:
             return True
         while n > 0:
             n /= 3
+            if int(n) != n:
+                return False
+            elif n == 1:
+                return True
+
+Leetcode power of four
+class Solution:
+    def isPowerOfFour(self, n: int) -> bool:
+        if n == 0:
+            return False
+        elif n == 1:
+            return True
+        while n > 0:
+            n /= 4
             if int(n) != n:
                 return False
             elif n == 1:
