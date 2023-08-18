969524ebc9c0c28822cc26a0c44ce48893fd819a
Will <66586152+gostjoke@users.noreply.github.com>
2023-08-18 00:42:55 -0700

    Create Leetcode 0414 Third Maximum Number.py

---
+++ Leetcode 0414 Third Maximum Number.py
@@ -0,0 +1,10 @@
+"""
+08/18/2023
+"""
+class Solution:
+    def thirdMax(self, nums: List[int]) -> int:
+        nums = sorted(list(set(nums)))
+        if len(nums) < 3: 
+            return nums[-1]
+        else:
+            return nums[-3] 
