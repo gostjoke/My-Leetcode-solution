06fede7ecdb5ace2e32f36d71917948b5395c5a6
Will <66586152+gostjoke@users.noreply.github.com>
2023-10-09 11:54:56 -0700

    Create Leetcode 0201 Bitwise AND of Numbers Range.py

---
+++ Leetcode 0201 Bitwise AND of Numbers Range.py
@@ -0,0 +1,14 @@
+"""
+10/09/2023
+"""
+
+class Solution:
+    def rangeBitwiseAnd(self, left: int, right: int) -> int:
+        shift = 0
+
+        while left < right:
+            left >>= 1
+            right >>= 1
+            shift += 1
+
+        return left << shift
