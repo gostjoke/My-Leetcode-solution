f12df9b1043e80f9cc8df443621b18b34ed9d102
Will <66586152+gostjoke@users.noreply.github.com>
2023-12-09 12:52:26 -0800

    Create Leecode 2634 Filter Elements from Array.js

---
+++ Leecode 2634 Filter Elements from Array.js
@@ -0,0 +1,15 @@
+/**
+  12/09/2023
+ * @param {number[]} arr
+ * @param {Function} fn
+ * @return {number[]}
+ */
+var filter = function(arr, fn) {
+    let result = [];
+    for (let i=0; i < arr.length;i++){
+        if (fn(arr[i], i)){
+        result.push(arr[i]);
+        }
+    }
+    return result;
+};
