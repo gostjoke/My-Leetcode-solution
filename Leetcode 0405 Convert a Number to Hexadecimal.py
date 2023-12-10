8412850c5165473d1e1a1b88b887d463df0ef424
Will <66586152+gostjoke@users.noreply.github.com>
2023-12-10 03:53:48 -0800

    Create Leetcode 0405 Convert a Number to Hexadecimal.py

---
+++ Leetcode 0405 Convert a Number to Hexadecimal.py
@@ -0,0 +1,11 @@
+class Solution:
+    def toHex(self, num: int) -> str:
+        hex ="0123456789abcdef"
+        ans=""
+        if num == 0: return "0"
+        elif num < 0:
+            num += 2**32
+        while num:
+            ans += hex[num%16]
+            num//=16
+        return ans[::-1]
