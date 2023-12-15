d21d2e3fb437bf099c35d063c63ba21e161fb40c
Will <66586152+gostjoke@users.noreply.github.com>
2023-12-15 14:20:39 -0800

    Create Leetcode 0392 Is Subsequence.py

---
+++ Leetcode 0392 Is Subsequence.py
@@ -0,0 +1,12 @@
+"""
+12/15/2023
+"""
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        p1 = p2 = 0
+        while p1 < len(s) and p2 < len(t):
+            if s[p1] == t[p2]:
+                p1 += 1
+            p2 += 1
+        if p1 == len(s):
+            return True
