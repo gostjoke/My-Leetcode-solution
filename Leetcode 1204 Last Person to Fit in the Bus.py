92eb3950cf4769bc79e03317dfb02810ca019f0f
Will <66586152+gostjoke@users.noreply.github.com>
2024-04-19 07:55:52 -0400

    Update Leetcode 1204 Last Person to Fit in the Bus.py

--- Leetcode 1204 Last Person to Fit in the Bus.py
+++ Leetcode 1204 Last Person to Fit in the Bus.py
@@ -3,21 +3,14 @@ import pandas as pd
 
 def last_passenger(df: pd.DataFrame) -> pd.DataFrame:
     df.sort_values(by="turn", inplace =True)
-
     total_weight = 0
-    df_len = len(df)-1
     last_person = ""
     for ind, rows in df.iterrows():
         total_weight += rows["weight"]
         if total_weight == 1000:
-            df = pd.DataFrame({"person_name":[rows["person_name"]]})
-            return df
+            return pd.DataFrame({"person_name":[rows["person_name"]]})
         elif total_weight > 1000:
-            df = pd.DataFrame({"person_name":[last_person]})
-            return df
-
-        elif ind == df_len: 
-            df = pd.DataFrame({"person_name":[rows["person_name"]]})
-            return df
-            
+            return pd.DataFrame({"person_name":[last_person]})
         last_person = rows["person_name"]
+
+    return pd.DataFrame({"person_name":[last_person]})
