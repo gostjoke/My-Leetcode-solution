3ce8e50fe08b09bfca648ee53943997afddcbe25
Will <66586152+gostjoke@users.noreply.github.com>
2024-06-04 14:40:15 -0700

    Update Leetcode 0537 Complex Number Multiplication.py

--- Leetcode 0537 Complex Number Multiplication.py
+++ Leetcode 0537 Complex Number Multiplication.py
@@ -5,13 +5,10 @@ class Solution:
             number_list = number.split("+")
             number_n, number_i = int(number_list[0]), int((number_list[1]).replace("i",""))
             return number_n, number_i
-
-        ans=""
         num1_n, num1_i = num_split(num1)
         num2_n, num2_i = num_split(num2)
         number_part = num1_n*num2_n + -1*(num1_i*num2_i)
         i_part = (num1_n * num2_i) + (num2_n * num1_i)
-        ans = f"{number_part}+{i_part}i"
+        return f"{number_part}+{i_part}i"
 
-        return ans
         
