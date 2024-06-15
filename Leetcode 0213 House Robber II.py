e49ee13a0a73cf190b3725c2b1d4fc606603aaa6
Will <66586152+gostjoke@users.noreply.github.com>
2024-06-15 13:38:17 -0700

    Create Leetcode 0213 House Robber II.py

---
+++ Leetcode 0213 House Robber II.py
@@ -0,0 +1,18 @@
+# 06152024
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        def houseRob(nums):
+            # as houseRober 1
+            dp = [0]*len(nums)
+            dp[0] = nums[0]
+            dp[1] = max(nums[0], nums[1])
+            for i in range(2, len(nums)):
+                dp[i] = max(dp[i-1], nums[i]+dp[i-2])
+
+            return dp[-1]
+
+        if len(nums) == 0: return 0
+        if len(nums) == 1: return nums[0]
+        if len(nums) == 2: return max(nums)
+
+        return max(houseRob(nums[:-1]), houseRob(nums[1:]))
