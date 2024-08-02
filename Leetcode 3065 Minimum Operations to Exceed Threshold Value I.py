2ef921258c64c3066c28ce198b8d29b3fd783ab0
Will <66586152+gostjoke@users.noreply.github.com>
2024-08-02 11:25:29 -0700

    Create Leetcode 3065 Minimum Operations to Exceed Threshold Value I.py

---
+++ Leetcode 3065 Minimum Operations to Exceed Threshold Value I.py
@@ -0,0 +1,8 @@
+# 08/02/2024
+class Solution:
+    def minOperations(self, nums: List[int], k: int) -> int:
+        count = 0
+        for i in nums:
+            if i < k:
+                count+=1
+        return count
