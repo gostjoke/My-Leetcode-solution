4f377a2633f2fcfcd43986381ca5db9cddfb916e
Will <66586152+gostjoke@users.noreply.github.com>
2025-06-12 01:50:03 -0700

    Update Leetcode 0036 Valid Sudoku.py
    forgot 2 years finally came back and finished it.

--- Leetcode 0036 Valid Sudoku.py
+++ Leetcode 0036 Valid Sudoku.py
@@ -3,46 +3,35 @@
 # -*- encoding: utf-8 -*-
 '''
 @date         :2023/04/25 15:53:15
+@date         :2025/06/12 finished....
 '''
+
 class Solution:
     def isValidSudoku(self, board: list[list[str]]) -> bool:
-        for row in range(9):
-            # row test
-            list_test = [i for i in board[row] if i != "."]
-            if len(set(list_test)) == len(list_test):
-                pass
-            else:
-                print(1)
-                return False
-            # col test
-            a = []
-            for col in range(9):
-
-                if board[col][row] not in a:
-                    a.append(board[col][row])
-                elif board[col][row] == ".":
-                    pass
-                else:
-                    print(2)
+        for i in range(9):
+            rtol = []
+            top = []
+            for j in range(9):
+                r = board[i][j]
+                if r !='.' and r in rtol:
                     return False
-            # from left cornor top to right bottom corner
-            b = []
+                elif r != '.':
+                    rtol.append(r)
+                t = board[j][i]
+                if t !='.' and t in top:
+                    return False
+                elif t != '.':
+                    top.append(t)    
+        for box_row in range(0, 9, 3):
+            for box_col in range(0, 9, 3):
+                seen = set()
+                for i in range(3):
+                    for j in range(3):
+                        val = board[box_row + i][box_col + j]
+                        if val != '.':
+                            if val in seen:
+                                return False
+                            seen.add(val)
 
-            if board[row][row] not in b:
-                b.append(board[row][row])
-            elif board[row][row] ==".":
-                pass
-            else:
-                print(3)
-                return False
-            ## form right cornor to left bottom corner
-            c= []
-            if board[row][8-row] not in c:
-                c.append(board[row][8-row])
-            elif board[row][8-row] == ".":
-                pass
-            else:
-                print(4)
-                return False
-            
-        return True
+        return True               
+                
