9dcd25d3e1be97d6157ef6d3ee61327d6d6dd0e3
Will <66586152+gostjoke@users.noreply.github.com>
2025-06-27 11:24:21 -0700

    Create Leetcode 0590 N-ary Tree Postorder Traversal.py

---
+++ Leetcode 0590 N-ary Tree Postorder Traversal.py
@@ -0,0 +1,19 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val: Optional[int] = None, children: Optional[List['Node']] = None):
+        self.val = val
+        self.children = children
+"""
+
+class Solution:
+    def postorder(self, root: 'Node') -> List[int]:
+        if not root:
+            return []
+        ans = []
+        def dfs(root):
+            for i in root.children:
+                dfs(i)
+            ans.append(root.val)
+        dfs(root)
+        return ans
