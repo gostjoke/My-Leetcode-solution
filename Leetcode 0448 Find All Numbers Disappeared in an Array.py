6b3f002a3ca146a2b0623f91151fe674fbeabac9
Will <66586152+gostjoke@users.noreply.github.com>
2025-06-30 00:29:29 -0700

    Update Leetcode 0448 Find All Numbers Disappeared in an Array.py

--- Leetcode 0448 Find All Numbers Disappeared in an Array.py
+++ Leetcode 0448 Find All Numbers Disappeared in an Array.py
@@ -1,5 +1,12 @@
 ## 20250630
-
+# O(n)  O(1)
+class Solution:
+    def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
+        for i in nums:
+            index = abs(i) - 1
+            nums[index] = abs(nums[index])*-1
+        return [ind+1 for ind, i in enumerate(nums) if i > 0]
+# O(n)  O(n)
 class Solution:
     def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
         return list(set(i+1 for i in range(len(nums))) - set(nums))
