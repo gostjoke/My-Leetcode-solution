2674dbc0a01eaa59e33adde6f50e4ba7af99caac
Will <66586152+gostjoke@users.noreply.github.com>
2025-07-16 10:23:54 -0700

    Update Leetcode 0242 Valid Anagram.py

--- Leetcode 0242 Valid Anagram.py
+++ Leetcode 0242 Valid Anagram.py
@@ -1,5 +1,17 @@
 #!/usr/bin/env python
 # -*- encoding: utf-8 -*-
+
+# 2025/07/16 update
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        count = [0] * 26
+        for c1, c2 in zip(s, t):
+            count[ord(c1) - ord('a')] += 1
+            count[ord(c2) - ord('a')] -= 1
+        return all(i == 0 for i in count )
+
 '''
 @date         :2023/06/05 11:15:57
 '''
