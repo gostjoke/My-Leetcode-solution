65fa0011129dd4a1e2809f67013034bec8cc789e
Will <66586152+gostjoke@users.noreply.github.com>
2025-08-08 23:53:15 -0700

    Create Leetcode 0771 Jewels and Stones.py

---
+++ Leetcode 0771 Jewels and Stones.py
@@ -0,0 +1,9 @@
+# 20250808
+class Solution:
+    def numJewelsInStones(self, jewels: str, stones: str) -> int:
+        count = 0
+        jewels = set(jewels)
+        for i in stones:
+            if i in jewels:
+                count+=1
+        return count
