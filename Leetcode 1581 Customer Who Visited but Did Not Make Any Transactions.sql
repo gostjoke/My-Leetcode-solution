861e55e38395d65ae8cc17f101e6c003f5fb33fc
Will <66586152+gostjoke@users.noreply.github.com>
2025-12-15 15:45:07 -0800

    Create Leetcode 1581 Customer Who Visited but Did Not Make Any Transactions.sql

---
+++ Leetcode 1581 Customer Who Visited but Did Not Make Any Transactions.sql
@@ -0,0 +1,5 @@
+# 2025/12/15
+# Write your MySQL query statement below
+select  customer_id, count(visit_id) as count_no_trans from Visits
+where visit_id not in (select visit_id from Transactions)
+group by customer_id
