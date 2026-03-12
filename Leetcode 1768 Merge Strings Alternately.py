011da8cf5c1c0691178b0fc898d9b0ade42671bf
Tien-Wei Hsu <66586152+gostjoke@users.noreply.github.com>
2026-03-12 05:34:19 -0700

    Add mergeAlternately method to merge two strings
    Implement mergeAlternately method to merge two strings alternately.

--- Leetcode 1768 Merge Strings Alternately.py
+++ Leetcode 1768 Merge Strings Alternately.py
@@ -1,3 +1,25 @@
+# 2026/03/12
+class Solution:
+    def mergeAlternately(self, word1: str, word2: str) -> str:
+        ans = ""
+        odd = 0
+        w1 = [i for i in word1]
+        w2 = [i for i in word2]
+        while w1 or w2:
+            if not w2:
+                ans += w1.pop(0)
+            elif not w1:
+                ans += w2.pop(0)
+            elif odd == 0:
+                ans += w1.pop(0)
+                odd = 1
+            else:
+                ans += w2.pop(0)
+                odd = 0
+        return ans             
+
+
+
 '''
 06/27/2023
 '''
