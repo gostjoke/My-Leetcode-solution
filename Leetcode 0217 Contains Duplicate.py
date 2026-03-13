c0d79d3033e6ec0750a1ef0f93da794a6ebccfe6
Tien-Wei Hsu <66586152+gostjoke@users.noreply.github.com>
2026-03-12 23:24:53 -0700

    Update Leetcode 0217 Contains Duplicate.py

--- Leetcode 0217 Contains Duplicate.py
+++ Leetcode 0217 Contains Duplicate.py
@@ -1,3 +1,16 @@
+# 2026/03/12
+class Solution:
+    def containsDuplicate(self, nums):
+        seen = set()
+
+        for n in nums:
+            if n in seen:
+                return True
+            seen.add(n)
+
+        return False
+
+
 #!/usr/bin/env python
 # -*- encoding: utf-8 -*-
 '''
@@ -8,5 +21,6 @@
 class Solution:
     def containsDuplicate(self, nums: list[int]) -> bool:
         return True if len(set(nums)) != len(nums) else False
+
 
 ## I alway use set() to remove duplicates, so I immediately think this
